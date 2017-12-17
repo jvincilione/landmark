68a411ee14102b655f5258d5c0c7a525b251889e
Jacques Vincilione <jvincilione@gmail.com>
2017-12-16 20:31:52 -0500

    Update config

--- api/db/config.php
+++ api/db/config.php
@@ -2,8 +2,8 @@
 
 try {
   $this->db = new PDO("mysql:host=". DB_HOST .";dbname=". DB_NAME .";port=". DB_PORT, DB_USER, DB_PASS);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $db->exec("SET NAMES 'utf8'");
+  $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $this->db->exec("SET NAMES 'utf8'");
 } catch(Exception $e) {
   echo "Could not connect to database at " . DB_HOST . "\n\n" . $e;
   exit;
