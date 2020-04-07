f2cf4d0933945c4a30bffe78f1a239666653a68b
Jacques Vincilione <jacques.vincilione@rakuten.com>
2020-04-07 16:01:25 +0900

    Safe urls

--- src/app/stream/stream.component.ts
+++ src/app/stream/stream.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stream',
   templateUrl: './stream.component.html',
-  styleUrls: ['./stream.component.scss']
+  styleUrls: ['./stream.component.scss'],
 })
 export class StreamComponent {
-  public readonly youtubeId = 'UCFUWKbJJw3_duv44jevTxWA';
+  public readonly youtubeUrl =
+    'https://www.youtube.com/embed/live_stream?channel=UCFUWKbJJw3_duv44jevTxWA';
 }
